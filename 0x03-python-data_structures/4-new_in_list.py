164c5188a5d5662872d50ee5d4957c04f5e8f286
Edix505 <101802740+Edix505@users.noreply.github.com>
2022-07-04 22:08:35 +0200

    4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,1 +1,7 @@
-
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    a_list = my_list[:]
+    if 0 <= idx or idx < len(a_list):
+        a_list[idx] = element
+        return (a_list)
+    return (my_list)
