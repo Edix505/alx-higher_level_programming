bf99094509a2bc5c850e8bda1e3b5e725f787e39
Edix505 <101802740+Edix505@users.noreply.github.com>
2022-07-04 22:17:25 +0200

    5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def no_a(my_string):
-    a_string = ""
-    for a in my_string:
-        if a != 'a' and a != 'A':
-            a_string += a
-    return a_string
+def no_c(my_string):
+    c_string = ""
+    for c in my_string:
+        if c != 'c' and a != 'C':
+            c_string += c
+    return c_string
