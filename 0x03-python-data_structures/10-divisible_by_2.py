af0e6fe5e06d6fcfbbf4874b61be680bea538b2e
Edix505 <101802740+Edix505@users.noreply.github.com>
2022-07-04 22:35:57 +0200

    10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,7 @@
-
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    d_list = []
+    if my_list:
+        for x in my_list:
+            d_list.append(False if x % 2 else True)
+    return d_list
