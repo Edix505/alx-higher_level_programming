88aef5be4a6e224c60ec68e7dff9682c6a0ac47d
Edix505 <101802740+Edix505@users.noreply.github.com>
2022-07-08 23:57:15 +0200

    12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     roman numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'L': 50, 'M': 1000}
     n = 0
     total = 0
-     if (roman_string is None) or (type(roman_string) is not str):
+     if isinstance(roman_string, str):
     
         for n in range(len(roman_string) - 1):
             if roman numerals[roman_string[n]] >= roman numerals[roman_string[n + 1]]:
@@ -14,4 +14,4 @@ def roman_to_int(roman_string):
         total += roman numerals[roman_string[n]]
         return total
     else:
-        return 0
+        return (0)
