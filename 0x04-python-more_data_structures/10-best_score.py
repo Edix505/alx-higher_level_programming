c9696bd36830608b0ebc84da1a1bc7e81adf0ee7
Edix505 <101802740+Edix505@users.noreply.github.com>
2022-07-08 23:41:50 +0200

    10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def best_score(a_dict):
+    if a_dict and len(a_dict):
+        maximum = list(a_dict.keys())[0]
+        for a in a_dict:
+            if a_dict[a] > a_dict[maximum]:
+                maximum = a
+        return maximum
+    return None
