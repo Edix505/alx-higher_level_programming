b1ddd23abbd28569f2baa4f27a20563c53686533
Edix505 <101802740+Edix505@users.noreply.github.com>
2022-07-18 21:50:22 +0200

    2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    n = 0
+    for a in range(x):
+        try:
+            print("{:d}".format(my_list[a]), end='')
+            n += 1
+        except (ValueError, TypeError):
+            x += 1
+    print('')
+    return n
