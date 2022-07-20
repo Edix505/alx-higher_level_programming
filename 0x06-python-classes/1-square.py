81ba0729cd3560b91b1e52c8b81e83123c95b725
Edix505 <101802740+Edix505@users.noreply.github.com>
2022-07-20 16:28:13 +0200

    1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square class definition"""
+
+
+class Square:
+    """class represents a square """
+
+    def __init__(self, size):
+        """Initializes a square
+        Args:
+            size (int): size of the square
+        Returns: None
+        """
+        self.__size = size
