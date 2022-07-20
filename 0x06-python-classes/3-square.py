5282a413e38d9adca3f5be19ec6d4d8018e0921b
Edix505 <101802740+Edix505@users.noreply.github.com>
2022-07-20 16:40:09 +0200

    3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""class Square defines square"""
+
+
+class Square:
+    """Represents a square
+    Attributes:
+        __size (int): size of a side of the square
+    """
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+
+    def area(self):
+        return (self.__size) ** 2
