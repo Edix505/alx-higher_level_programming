1b7a4afc6a8dbe671cf13dfd6ceb68802af32594
Edix505 <101802740+Edix505@users.noreply.github.com>
2022-08-01 21:45:25 +0200

    2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''module 2-is_same_class.py'''
+
+
+def is_same_class(obj, a_class):
+    '''this fun returns true if obj is instance of a_class'''
+    return type(obj) is a_class
