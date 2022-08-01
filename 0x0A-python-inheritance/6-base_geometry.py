07700cbe03542413794b6db3c39fcfd8a29e2572
Edix505 <101802740+Edix505@users.noreply.github.com>
2022-08-01 21:52:43 +0200

    6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''module 6-base_geometry.py'''
+
+
+class BaseGeometry:
+    '''emty class'''
+    def __init__(self):
+        '''emty init'''
+        pass
+
+    def area(self):
+        '''useless function'''
+        raise Exception('area() is not implemented')
