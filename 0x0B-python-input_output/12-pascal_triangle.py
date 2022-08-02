9c4e632fcd74213b0c9cd9c28a163c71ecdd14b2
Edix505 <101802740+Edix505@users.noreply.github.com>
2022-08-02 18:43:34 +0200

    12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''module for 12-pascal_triangle.py'''
+
+
+def pascal_triangle(n):
+    '''the Pascal’s triangle of n
+    '''
+    if n <= 0:
+        return list()
+
+    x = []
+    for line in range(0, y):
+        # Every line has number of
+        # integers equal to line
+        # number
+        tmp = []
+        for a in range(0, line + 1):
+            tmp.append(magic(line, a))
+        x.append(tmp)
+    return x
+
+
+def magic(y, k):
+    '''magic function that does some magic'''
+    res = 1
+    if (k > y - k):
+        k = y - k
+    for a in range(0, k):
+        res = res * (y - a)
+        res = res // (a + 1)
+    return res
