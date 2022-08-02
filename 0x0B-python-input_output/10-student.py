cd735ffbc82f22029023f7cb77f5c5b690364035
Edix505 <101802740+Edix505@users.noreply.github.com>
2022-08-02 18:36:45 +0200

    10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''module for 10-student.py'''
+
+
+class Student:
+    '''student class'''
+    def __init__(self, first_name, last_name, age):
+        '''register the student'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''return a dictionary representation of a Student
+        '''
+        try:
+            for attr in attrs:
+                if type(attr) is not str:
+                    return self.__dict__
+        except Exception:
+            return self.__dict__
+        MD = dict()
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                MD[key] = value
+        return MD
