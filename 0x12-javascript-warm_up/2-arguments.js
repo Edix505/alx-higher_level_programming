1fe155a53ecda9d8be7568ff9bad512eee74dead
Edix505 <101802740+Edix505@users.noreply.github.com>
2022-10-04 00:35:54 +0200

    2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 const process = require('process');
+let message;
 if (process.argv.length === 3) { message = 'Argument found'; } else if (process.argv.length < 3) { message = 'No argument';} else
 {
   message = 'Arguments found';
